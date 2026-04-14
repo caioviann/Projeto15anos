d4040034735683b4d2ce1b60ee2c50cf0d487e4f
caio henrique <caiohenriqueviana01@gmail.com>
2026-04-14 12:42:09 -0300

    adding readme update

---
+++ projeto15anos/src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+import LogoComponent from './components/Logo/LogoComponent'
+
+function App() {
+  return (
+    <LogoComponent/>
+  )
+}
+
+export default App
---
+++ projeto15anos/src/components/Logo/LogoComponent.tsx
@@ -0,0 +1,11 @@
+import "./style.css"
+
+function LogoComponent() {
+    return (
+        <div>
+            <p>Camilly Cristina Nunes Viana</p>
+        </div>
+    )
+}
+
+export default LogoComponent;
